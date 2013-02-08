800c2c6c1925b6292ed2438f51d2f5bd8ac635e4
gdeyoung <gdeyoung@hansondodge.com>
2013-02-08 16:51:47 -0600

    Added TShirtSize enum

---
+++ src/DirtyGirl.Models/Enums/TShirtSize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DirtyGirl.Models.Enums
+{
+    public enum TShirtSize
+    {
+        S,
+        M,
+        L,
+        XL,
+        XXL
+    }
+}
