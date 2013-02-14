3912edf2a21fbf83cec9f888fc67a115c499d380
gdeyoung <gdeyoung@hansondodge.com>
2013-02-14 10:53:24 -0600

    Updated enums.TShirtSize details

--- src/DirtyGirl.Models/Enums/TShirtSize.cs
+++ src/DirtyGirl.Models/Enums/TShirtSize.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace DirtyGirl.Models.Enums
 {
     public enum TShirtSize
     {
-        S,
-        M,
-        L,
-        XL,
-        XXL
+        [Display(Name = "Small", Description = "Small")]
+        Small = 1,
+        [Display(Name = "Medium", Description = "Medium")]
+        Medium = 2,
+        [Display(Name = "Large", Description = "Large")]
+        Large = 3,
+        [Display(Name = "XLarge", Description = "XLarge")]
+        XLarge = 4,
+        [Display(Name = "XXLarge", Description = "XXLarge")]
+        XXLarge = 5
     }
 }
