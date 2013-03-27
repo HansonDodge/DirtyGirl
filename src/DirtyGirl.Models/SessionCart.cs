c68ca4214414cd392ce491769199727754d9bf4c
gdeyoung <gdeyoung@hansondodge.com>
2013-03-27 08:50:07 -0500

    Changed CheckOutFocus from CartFocusType to CartFocusType[] in SessionCart.cs

--- src/DirtyGirl.Models/SessionCart.cs
+++ src/DirtyGirl.Models/SessionCart.cs
@@ -8,9 +8,20 @@ namespace DirtyGirl.Models
 {
     public class SessionCart
     {
+        private CartFocusType[] _foci;
+
         public Dictionary<Guid, ActionItem> ActionItems { get; set; }
 
-        public CartFocusType CheckOutFocus { get; set; }
+        public CartFocusType[] CheckOutFocus
+        {
+            get {
+                if (_foci == null) {
+                    _foci = new CartFocusType[]{};
+                }
+                return _foci;
+            }
+            set;
+        }
 
         public string ResultingConfirmationCode { get; set; }
                 
